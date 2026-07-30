d2bcddd58e2b6b7c1225a2e1a79b1d9407d9db30
Miguel Angel Alavazarez Hernandez <223253@ids.upchiapas.edu.mx>
2026-07-30 04:43:53 -0600

    Actualizacion

--- api/src/features/comments/application/CreateCommentUseCase.go
+++ api/src/features/comments/application/CreateCommentUseCase.go
@@ -1,4 +1,5 @@
 package application
+import "log"
 
 import (
 	"context"
@@ -32,6 +33,9 @@ func (uc *CreateCommentUseCase) Execute(ctx context.Context, postID string, user
 	if err != nil {
 		return response.CommentResponse{}, err
 	}
+
+	log.Printf("Moderation result: %+v\n", result)
+
 	if result.IsToxic {
 		return response.CommentResponse{}, moderation.ErrToxicContent
 	}
